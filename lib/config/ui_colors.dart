fd856f34147bb846569bccf655dc7afb69b7ae85
Haroune-REZKI <haroune.rezki@ensia.edu.dz>
2024-07-19 17:15:33 +0100

    added the AppColors class

---
+++ lib/config/ui_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // these are the main app colors
+  static const Color main = Color(0xFF00ABB6);
+  static const Color mainDark = Color(0xFF3E3B3B);
+
+  // background color
+  static const Color white = Color(0xFFFFFFFF);
+
+  // These are secondary colors
+  static const Color secondaryColor1 = Color(0xFFFFA487);
+  static const Color secondaryColor2 = Color(0xFF4BA1FF);
+  static const Color secondaryColor3 = Color(0xFF47CFD6);
+
+  // those are colors in texts
+  static const Color light = Color(0xFF868E96);
+  static const Color dark = Color(0xFF000000);
+}
